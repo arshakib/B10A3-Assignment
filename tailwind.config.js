fe7160469e1e62d2c8edef83a89259f0dbf5ac26
shakib <atking2240@gmail.com>
2024-08-30 00:09:52 +0600

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'addiback': "url('./assets/bg.jpg')"
+        }
+  
+      },
     },
     plugins: [],
   }
